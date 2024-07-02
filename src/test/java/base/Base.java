59d1319dea535f1db6bed9795701bcc7b70e2e40
Ahmed Gharam <168560878+AhmedGharam@users.noreply.github.com>
2024-07-02 18:19:16 +0300

    first commit

---
+++ src/test/java/base/Base.java
@@ -0,0 +1,31 @@
+package base;
+
+import Pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+
+    protected WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+    }
+
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+}
